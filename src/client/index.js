484b07d077566f6b22ea44e1d3c6cbcca9aa2a64
dmayes77 <dmayes77@gmail.com>
2020-04-08 17:17:50 -0400

    chore: cleaned up code for new project

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,11 @@
-import { checkForName } from './js/nameChecker'
-import { handleSubmit } from './js/formHandler'
+import app from './js/app';
 
-console.log(checkForName);
+import './styles/base.scss';
+import './styles/footer.scss';
+import './styles/form.scss';
+import './styles/header.scss';
+import './styles/resets.scss';
 
-alert("I EXIST")
-console.log("CHANGE!!");
+alert('i am running');
+
+export { app };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,23 +1,37 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    entry: './src/client/index.js',
-    mode: 'production',
-    module: {
-        rules: [
-            {
-                test: '/\.js$/',
-                exclude: /node_modules/,
-                loader: "babel-loader"
-            }
-        ]
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: "./src/client/views/index.html",
-            filename: "./index.html",
-        })
-    ]
-}
+  entry: './src/client/index.js',
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: '/.js$/',
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/client/views/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({ filename: '[name].css' }),
+  ],
+};
