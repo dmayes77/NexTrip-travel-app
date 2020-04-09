b143ae3ce5b4cffafcaf963b366109e215ca5d6a
dmayes77 <dmayes77@gmail.com>
2020-04-09 11:51:48 -0400

    chore: completed header

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,9 @@
 import app from './js/app';
 
-import './styles/base.scss';
+import './styles/resets.scss';
+import './styles/_base.scss';
 import './styles/footer.scss';
 import './styles/form.scss';
 import './styles/header.scss';
-import './styles/resets.scss';
-
-alert('i am running');
 
 export { app };
