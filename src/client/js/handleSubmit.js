22fea8f962dfadc1aaa9b7a5ff4e5827a93a1beb
dmayes77 <dmayes77@gmail.com>
2020-04-22 13:23:16 -0400

    refctor: added to handle error with user input

--- src/client/js/handleSubmit.js
+++ src/client/js/handleSubmit.js
@@ -1,15 +1,45 @@
 function handleSubmit(event) {
   event.preventDefault();
+  clearInnerHTML();
+
   const url = 'http://localhost:8080';
   // check what text was put into the form field
-  let location = document.getElementById('location').value;
+  let city = document.getElementById('location').value.split(', ')[0];
+  let state = document.getElementById('location').value.split(', ')[1];
   let date = document.getElementById('date').value;
 
-  console.log('::: Form Submitted :::');
+  console.log('::: Form Submitted :::', city, state);
 
-  Client.getData(url, location)
-    .then((data) => Client.postData(`${url}/travel`, data))
-    .then((data) => Client.updateUI(data, date));
+  if (state != undefined) {
+    checkDate(date)
+      ? (document.querySelector('#error').innerHTML =
+          'Please enter a date that has not passed!')
+      : Client.getData(url, city, state)
+          .then((data) => Client.postData(`${url}/travel`, data))
+          .then((data) => Client.updateUI(data, date));
+  } else {
+    let errMsg = 'Please enter valid US city and state! ex. Atlanta, GA';
+    document.querySelector('#error').innerHTML = errMsg;
+    document.getElementById('location').value = '';
+  }
+}
+
+function checkDate(date) {
+  let selectedDate = new Date(date);
+  let now = new Date();
+  if (selectedDate <= now) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function clearInnerHTML() {
+  let d = document.getElementById('error');
+  if (d.innerHTML.length > 0) {
+    console.log('remove');
+    d.innerHTML = '';
+  }
 }
 
 export { handleSubmit };
